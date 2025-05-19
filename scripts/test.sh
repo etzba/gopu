c59ced9072018ec23c2bc41f27676fd86b05717a
Nadav Ben Mazia <nadavbm@outlook.com>
2025-05-19 23:11:07 +0200

    extend post method and tests for locations

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,3 @@
-etz api --exec=scripts/executions.yaml -w 5 -r 5 -d 3s
-sleep 5
 etz api --exec=scripts/locations.yaml -w 5 -r 5 -d 3s
 sleep 5
 etz api --exec=scripts/calc.yaml -w=5 -r=5 -d=3s
--- server/locations.go
+++ server/locations.go
@@ -1,9 +1,12 @@
 package server
 
 import (
+	"encoding/json"
 	"net/http"
 	"strconv"
 	"strings"
+
+	"github.com/etzba/gopu/wire"
 )
 
 func (s *Server) getLocations() func(w http.ResponseWriter, r *http.Request) {
@@ -16,6 +19,22 @@ func (s *Server) getLocations() func(w http.ResponseWriter, r *http.Request) {
 func (s *Server) postLocation() func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		s.Logger.Info("Server go request" + " method: " + r.Method + " uri: " + r.RequestURI)
+		loc := wire.Location{}
+		if err := json.NewDecoder(r.Body).Decode(&loc); err != nil {
+			s.Logger.Error("Failed to create new decoder", err)
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		location := wire.Location{
+			Name:       loc.Name,
+			Address:    loc.Address,
+			Longtitude: loc.Longtitude,
+			Latitude:   loc.Latitude,
+		}
+
+		locations = append(locations, location)
+		s.Logger.Info("Add a new location to memory " + loc.Name)
 		s.Respoder.SendOK(w)
 	}
 }
