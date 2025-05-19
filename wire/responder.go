aa970b6b3fdc3799405b216d0a6f5c2dfb2350b2
Nadav Ben Mazia <nadavbm@outlook.com>
2025-05-19 22:52:41 +0200

    Feat(wire): Extend wire package

--- wire/responder.go
+++ wire/responder.go
@@ -1,13 +1,14 @@
 package wire
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/etzba/gopu/pkg/logger"
 )
 
 type Responder interface {
-	SendOK(w http.ResponseWriter)
+	SendOK(w http.ResponseWriter, obj ...interface{})
 	SendNothing(w http.ResponseWriter)
 	SendError(w http.ResponseWriter, err error)
 	SendBadRequest(w http.ResponseWriter)
@@ -17,9 +18,27 @@ type Respond struct {
 	Logger *logger.Log
 }
 
-func (r Respond) SendOK(w http.ResponseWriter) {
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("OK!"))
+func (r Respond) SendOK(w http.ResponseWriter, obj ...interface{}) {
+	if obj != nil {
+		body, err := json.Marshal(obj)
+		if err != nil {
+			r.Logger.Error("could not marshal", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			res := []byte(`{"message":"could not marshal"}`)
+			_, err = w.Write(res)
+			if err != nil {
+				r.Logger.Error("could not write data", err)
+			}
+		}
+		w.Header().Set("Content-Type", "Application/json")
+		w.Header().Set("X-Frame-Options", "SAMEORIGIN")
+		_, err = w.Write(body)
+		if err != nil {
+			r.Logger.Error("could not write body", err)
+		}
+	} else {
+		w.Write([]byte("OK!"))
+	}
 }
 
 func (r Respond) SendNothing(w http.ResponseWriter) {
