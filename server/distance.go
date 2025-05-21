292afd85ea1d9fb7b1a064af7fa82cfc00ec5744
Nadav Ben Mazia <nadavbm@outlook.com>
2025-05-21 23:01:37 +0200

    add distance endpoint and tests

--- server/distance.go
+++ server/distance.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"encoding/json"
+	"fmt"
 	"math"
 	"net/http"
 
@@ -10,7 +12,37 @@ import (
 func (s *Server) getDistance() func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		s.Logger.Info("Server go request" + " method: " + r.Method + " uri: " + r.RequestURI)
-		s.Respoder.SendOK(w)
+		dis := wire.Distance{}
+		if err := json.NewDecoder(r.Body).Decode(&dis); err != nil {
+			s.Logger.Error("Failed to create new decoder", err)
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		wireDistance := wire.Distance{
+			Formula:    dis.Formula,
+			FromPlace:  dis.FromPlace,
+			Longtitude: dis.Longtitude,
+			Latitude:   dis.Latitude,
+		}
+		place := wire.Location{}
+		for _, p := range locations {
+			if dis.FromPlace == p.Name {
+				place.Name = p.Name
+				place.Latitude = p.Latitude
+				place.Longtitude = p.Longtitude
+			}
+		}
+		if place.Name == "" {
+			s.Respoder.SendBadRequest(w)
+			return
+		}
+
+		distance := getDistanceByPythagoras(place, wireDistance) * 100
+		obj := make(map[string]float64, 1)
+		obj["distance"] = distance
+		s.Logger.Info(fmt.Sprintf("Client located about %fkm from %s", distance, place.Name))
+		s.Respoder.SendOK(w, obj)
 	}
 }
 
@@ -18,7 +50,7 @@ func (s *Server) getDistance() func(w http.ResponseWriter, r *http.Request) {
 // --------------------------------------------------------------------- helpers ----------------------------------------------------------
 //
 
-func getDistanceByPythagoras(loc1, loc2 wire.Location) float64 {
+func getDistanceByPythagoras(loc1 wire.Location, loc2 wire.Distance) float64 {
 	x := (loc1.Latitude - loc2.Latitude) * (loc1.Latitude - loc2.Latitude)
 	y := (loc1.Longtitude - loc2.Longtitude) * (loc1.Longtitude - loc2.Longtitude)
 	return math.Sqrt(x + y)
